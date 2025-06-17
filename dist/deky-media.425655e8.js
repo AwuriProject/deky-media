8e231a7b3795e2f735741685b19e5e5939fccb2d
AwuriProject <ngoziawurihappy@gmail.com>
2025-06-17 14:23:06 +0100

    Initial commit

---
+++ dist/deky-media.425655e8.js
@@ -0,0 +1,18 @@
+const list = document.querySelector(".list");
+const hamburgerMenu = document.querySelector(".menu");
+const open = document.querySelector(".open");
+const close = document.querySelector(".close");
+hamburgerMenu.addEventListener("click", ()=>{
+    open.classList.toggle("hidden");
+    close.classList.toggle("show");
+    list.classList.toggle("active");
+});
+// FAQ
+document.querySelectorAll(".faq__question").forEach((button)=>{
+    button.addEventListener("click", ()=>{
+        const answer = button.nextElementSibling;
+        answer.classList.toggle("active");
+    });
+});
+
+//# sourceMappingURL=deky-media.425655e8.js.map
